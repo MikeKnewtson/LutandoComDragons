04063ff0d6da0670bf7c529372e4873cdedf97f7
Mike Knewtson <mike@knewtson.com>
2024-10-21 14:19:26 -0300

    Refactor to extract heals

--- lib/Dragon.hpp
+++ lib/Dragon.hpp
@@ -9,16 +9,20 @@ private:
     static constexpr int healing_time = 3600;
     int health{max_health};
 
+    void heals(const Time& time) {
+        health += (time.seconds * max_health) / healing_time;
+        if (health > max_health) {
+            health = max_health;
+        }
+    }
+
 public:
     void takes_a_hit(const Weapon& weapon) {
         health -= weapon.damage;
     }
 
     void has_time(const Time& time) {
-        health += (time.seconds * max_health) / healing_time;
-        if (health > max_health) {
-            health = max_health;
-        }
+        heals(time);
     }
 
     [[nodiscard]] constexpr bool is_alive() const { return health > 0; }
