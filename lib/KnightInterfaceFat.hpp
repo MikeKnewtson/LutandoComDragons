dfc9d21b904b599ce37cef2cc1735e8824e5e4cf
Mike Knewtson <mike@knewtson.com>
2024-10-27 18:29:08 -0300

    Change the interface to make it more clear why it does not follow the Open/Closed Pinciple

--- lib/KnightInterfaceFat.hpp
+++ lib/KnightInterfaceFat.hpp
@@ -7,7 +7,8 @@ public:
   virtual void Scream() = 0;
   virtual void RunAway() = 0;
   virtual void Block() = 0;
-  virtual void Attack() = 0;
+  virtual void AttackWithSword() = 0;
+  virtual void AttachwithAxe() = 0;
   virtual void Die() = 0;
   virtual void Engage(DragonInterface *dragon) = 0;
 };
