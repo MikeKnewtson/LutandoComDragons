037fb729614843e07197a76baff53da753783ffd
Mike Knewtson <mike@knewtson.com>
2024-10-28 15:19:01 -0300

    A Knight's weapon can be static const for now.

--- lib/Knight.hpp
+++ lib/Knight.hpp
@@ -10,5 +10,5 @@ static constexpr auto knight_healing_time = 3600;
 template <typename WeaponType = Sword>
 requires std::is_base_of_v<HumanWeapon, WeaponType> class Knight
     : public MortalCreature<knight_max_health, knight_healing_time> {
-  WeaponType weapon;
+  static const WeaponType weapon;
 };
